a469440c1b944f55fbd26d489663dea1e6666d5d
Solman252 <undertalelover909@gmail.com>
2026-07-13 20:13:34 +1200

    add linix support for the customasm

--- customasm.py
+++ customasm.py
@@ -1,4 +1,4 @@
-import sys, subprocess, os, pyperclip
+import sys, subprocess, os
 from enum import Enum
 
 class AssemblyOutputType(Enum):
@@ -6,26 +6,27 @@ class AssemblyOutputType(Enum):
     BIN = 'Binary'
     HEX = 'Hexadecimal'
 
-PATH = ''
-
 def assemble(output_type: AssemblyOutputType = AssemblyOutputType.BIN, assembly: str | None = None, input_file: str | None = None):
     delete_inp = False
     if input_file is None:
         if assembly is not None:
-            with open(PATH+'input.customasmbuild','w') as f: f.write(assembly)
-            input_file = PATH+'input.customasmbuild'
+            with open('input.customasmbuild','w') as f: f.write(assembly)
+            input_file = 'input.customasmbuild'
             delete_inp = True
         else:
             raise ValueError('If an input file is not provided, a string with assembly code must be.')
     elif assembly is not None:
         raise ValueError('If an input file is provided, `assembly` argument must be None.')
     
-    subprocess.run([PATH+'customasm.exe', '-f', 'binstr', '-o', PATH+'build.customasmbuild', input_file], check=True, stdout=subprocess.PIPE)
+    if sys.platform == 'win32':
+        subprocess.run(['customasm.exe', '-f', 'binstr', '-o', 'build.customasmbuild', input_file], check=True, stdout=subprocess.PIPE)
+    else:
+        subprocess.run(['wine', 'customasm.exe', '-f', 'binstr', '-o', 'build.customasmbuild', input_file], check=True, stdout=subprocess.PIPE)
+
     if delete_inp:os.remove(input_file)
 
-    with open(PATH+'build.customasmbuild','r') as f: contents = f.read()
-    os.remove(PATH+'build.customasmbuild')
-    # edited = contents+('0'*(4096-len(contents)))
+    with open('build.customasmbuild','r') as f: contents = f.read()
+    os.remove('build.customasmbuild')
 
     binary_out = '\n'.join(contents[i:i+56] for i in range(0, len(contents), 56))
     if output_type == AssemblyOutputType.BIN:
@@ -41,59 +42,5 @@ def assemble(output_type: AssemblyOutputType = AssemblyOutputType.BIN, assembly:
 
     if output_type == AssemblyOutputType.BOTH: return (binary_out, hex_out)
     return hex_out
-
-if __name__ == '__main__':
-    
-    output_type = AssemblyOutputType.BOTH
-    copy = False
-    save = False
-    inp_file = None
-
-    for i,argument in enumerate(sys.argv):
-        if i == 0: continue
-
-        if argument in ['-output-type=hex','-output-type=hexadecimal']:
-            output_type = AssemblyOutputType.HEX
-        elif argument in ['-output-type=bin','-output-type=binary']:
-            output_type = AssemblyOutputType.BIN
-        elif argument in ['-output-type=both','-output-type=hex,bin','-output-type=bin,hex']:
-            output_type = AssemblyOutputType.BOTH
-
-        elif argument == '-copy':
-            copy = True
-        elif argument == '-save':
-            save = True
-        
-        elif argument.startswith('-file='):
-            inp_file = argument.removeprefix('-file=')
-        else:
-            if sys.argv[i-1] == '-file=':
-                inp_file = argument.removeprefix('-file=')
-            else:
-                raise ValueError(f'Unknown argument \'{sys.argv[i]}\'.')
-        
-    if inp_file == None: raise FileNotFoundError('No file was passed!')
-    inp_file = inp_file.replace('\\','/')
-
-    output = assemble(output_type,input_file=inp_file)
-    if save:
-        if output_type == AssemblyOutputType.BOTH:
-            with open('assembled.custom_asm_bin','w') as f: f.write(output[0])
-            print('Binary output saved as \'assembled.custom_asm_bin\'')
-            with open('assembled.custom_asm_hex','w') as f: f.write(output[1])
-            print('Hexadecimal output saved as \'assembled.custom_asm_hex\'')
-        else:
-            with open(f'assembled.custom_asm_{output_type.value[:3].lower()}','w') as f: f.write(output)
-            print(f'{output_type.value} output saved as \'assembled.custom_asm_{output_type.value[:3].lower()}\'')
-    
-    if copy:
-        if output_type == AssemblyOutputType.BOTH:
-            pyperclip.copy(output[0])
-            print('DLS ROM (Binary) copied to clipboard.')
-            pyperclip.copy(output[1])
-            print('DLS ROM (Hexadecimal) copied to clipboard.')
-        else:
-            pyperclip.copy(output)
-            print(f'DLS ROM ({output_type.value}) copied to clipboard.')
 
 __all__ = ['assemble','AssemblyOutputType']
